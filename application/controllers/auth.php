efcd47cbc1ba0d9413f94885b124b322805ab109
Alfonso9 <lorenzo_5_10@hotmail.com>
2015-06-02 10:23:23 -0500

    ..

--- application/controllers/auth.php
+++ application/controllers/auth.php
@@ -6,12 +6,12 @@ class Auth extends CI_Controller {
 	{
 		parent::__construct();
 		$this->form_validation->set_error_delimiters($this->config->item('error_start_delimiter', 'ion_auth'), $this->config->item('error_end_delimiter', 'ion_auth'));
+		//$this->_example_output((object)array('output' => '' , 'js_files' => array() , 'css_files' => array()));
 	}
 
 	//redirect if needed, otherwise display the user list
 	function index()
 	{
-		$this->_example_output((object)array('output' => '' , 'js_files' => array() , 'css_files' => array()));
 
 		if (!$this->ion_auth->logged_in())
 		{
