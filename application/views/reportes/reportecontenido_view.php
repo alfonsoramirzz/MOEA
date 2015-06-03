22d0dc8bf48c934418eca131a752df00d07bf4c3
Clagunes <clagunes25@gmail.com>
2015-06-03 12:50:04 -0500

    Modificación de estilo de la vista reportecontenido_view.php

--- application/views/reportes/reportecontenido_view.php
+++ application/views/reportes/reportecontenido_view.php
@@ -4,10 +4,55 @@
   <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="Content-type" content="text/html;charset=UTF-8">
-        <script type="text/javascript" src="<?= base_url(); ?>recursos/js/jquery-2.1.4.js); ?>"></script>
-        <script type="text/javascript" src="<?= base_url(); ?>recursos/js/bootstrap.js); ?>"></script>
-        <link rel="stylesheet" href="<?= echo base_url("recursos/css/bootstrap.css"); ?>"/>
-  
+        
+        <?php $this->load->helper('url');?>
+        <link rel="stylesheet" href="<?php echo base_url("recursos/css/bootstrap.css"); ?>"/>
+        <style type="text/css">
+  .reporteaula
+  {
+    margin-left: 50px;
+    margin-right: 50px;
+  }
+
+  .tftable 
+  {
+    font-size:10px;
+    color:#333333;
+    width:75%;
+    border-width: 1px;
+    border-color: black;
+    border-collapse: collapse;
+  }
+
+  .tftable th 
+  {
+    font-size:12px;
+    background-color:#009999;
+    border-width: 1px;
+    padding: 8px;
+    border-style: solid;
+    border-color: black;
+    text-align:center;
+    color: white;
+    
+  }
+
+  .tftable tr 
+  {
+    background-color:#ffffff;
+  }
+
+  .tftable td 
+  {
+    font-size:11px;
+    border-width: 1px;
+    padding: 8px;
+    border-style: solid;
+    border-color: black;
+    
+  }
+
+  </style>
   </head>
   <body>
     <div class="container">
@@ -22,7 +67,8 @@
         <h2><p class="text-center">Reporte de convocatorias por grado de estudios</p></h2>
       </div>
       <div class="table-responsive">
-        <table class="table table-bordered table-hover" align="center">
+        <!--<table class="table table-bordered table-hover" align="center">-->
+        <table class="tftable" border="1">
             <thead>
                 <tr>
                     <th align="center">Convocatoria</th>
@@ -55,5 +101,7 @@
         </table>
       </div>
     </div>
+    <script type="text/javascript" src="<?php echo base_url("recursos/js/jquery-2.1.4.js"); ?>"></script>
+    <script type="text/javascript" src="<?php echo base_url("recursos/js/bootstrap.js"); ?>"></script>
   </body>
 </html>
